c7c332011a5f4b3dfb79b127878e0d7eff386b79
Shivammali <malishivam34@gmail.com>
2021-02-18 18:58:42 +0530

    Created the files

---
+++ DiamondMerchant/src/main/java/com/diamondMerchant/main/DiamondMerchantApplication.java
@@ -0,0 +1,13 @@
+package com.diamondMerchant.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiamondMerchantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiamondMerchantApplication.class, args);
+	}
+
+}
