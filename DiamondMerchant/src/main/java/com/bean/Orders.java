ba1c3cde0046be3c71459a0057c290b08b5f5b88
Alien-Reborn <yashwanthgyash@gmail.com>
2021-02-19 09:56:38 +0530

    Diamond Merchant Added

---
+++ DiamondMerchant/src/main/java/com/bean/Orders.java
@@ -0,0 +1,75 @@
+package com.bean;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
+public class Orders {
+	
+	@Id
+	private int oid;
+	private String pname;
+	private int oqty;
+	private float oprice;
+	private LocalDate odate;
+	private String ostatus;
+	
+	public int getOid() {
+		return oid;
+	}
+	
+	public void setOid(int oid) {
+		this.oid = oid;
+	}
+	
+	public String getPname() {
+		return pname;
+	}
+	
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	
+	public int getOqty() {
+		return oqty;
+	}
+	
+	public void setOqty(int oqty) {
+		this.oqty = oqty;
+	}
+	
+	public float getOprice() {
+		return oprice;
+	}
+	
+	public void setOprice(float oprice) {
+		this.oprice = oprice;
+	}
+	
+	public LocalDate getOdate() {
+		return odate;
+	}
+	
+	public void setOdate(LocalDate odate) {
+		this.odate = odate;
+	}
+	public String getOstatus() {
+		return ostatus;
+	}
+	
+	public void setOstatus(String ostatus) {
+		this.ostatus = ostatus;
+	}
+	
+	@Override
+	public String toString() {
+		return "Orders [oid=" + oid + ", pname=" + pname + ", oqty=" + oqty + ", oprice=" + oprice + ", odate=" + odate
+				+ ", ostatus=" + ostatus + "]";
+	}
+
+	
+}
---
+++ Yashwant's data/SpringBootWithBillCURDOperation/SpringBootWithBillCURDOperation/src/main/java/com/bean/Customer.java
@@ -0,0 +1,49 @@
+package com.bean;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="customer")
+public class Customer {
+	@Id
+	@Column(name="customerid")
+	private int customerId;
+	private String name;
+	private String gender;
+	@Column(name="phonenumber")
+	private String phoneNumber;
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	@Override
+	public String toString() {
+		return "Customer [customerId=" + customerId + ", name=" + name + ", gender=" + gender + ", phoneNumber="
+				+ phoneNumber + "]";
+	}
+
+
+}
