573319c6ed96303e77f247f4ed556a303bda21a6
Shivammali <malishivam34@gmail.com>
2021-03-10 20:35:11 +0530

    Added customer, login and signup files

---
+++ DIAMOND-MERCHANT/FRONTEND/DiamondMerchant-UI/src/app/customer.model.ts
@@ -0,0 +1,10 @@
+export class Customer {
+  constructor(
+    public customerid?: number,
+    public name?: string,
+    public cemail?: string,
+    public gender?: string,
+    public phonenumber?: number,
+    public password?: string
+  ) {}
+}
